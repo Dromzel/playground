c5505df670645d50b608856a2a26cae17e13c745
Marcel Voigt <mvo@hup.de>
2018-11-07 23:58:43 +0100

    Zweite Activity, Wechsel Ansicht, Lambda Test

---
+++ app/src/main/java/de/mvo/playground/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package de.mvo.playground;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        final Intent intent = getIntent();
+        final String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        final TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/de/mvo/playground/MainActivity.java
+++ app/src/main/java/de/mvo/playground/MainActivity.java
@@ -1,13 +1,30 @@
 package de.mvo.playground;
 
+import android.content.Intent;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "PLAYGROUND.EXTRA.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something
+        final Intent intent = new Intent(this, DisplayMessageActivity.class);
+        final EditText editText = findViewById(R.id.editText);
+        final String message = editText.getText().toString();
+        // Test Lambdas
+        intent.putExtra(EXTRA_MESSAGE, message.isEmpty() ? "Keine Eingabe" : message);
+        startActivity(intent);
     }
 }
